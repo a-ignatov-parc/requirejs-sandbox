63724ce105c0c58262dfd5d6c54487285878c1bc
Anton Ignatov <abietis@gmail.com>
2013-07-31 01:47:17 +0400

    Сделан небольшой рефакторинг тестов

--- tests/module.js
+++ tests/module.js
@@ -86,6 +86,27 @@ asyncTest('loading requirejs-sandbox module', 1, function() {
 			callback: function(require) {
 				var sandboxApi = this;
 
+				test('resoving url to file by it\'s name', function() {
+					var options = {
+							baseUrl: 'app/static/js',
+							paths: {
+								'css': '../../styles/css',
+								'backbone': '../libs/backbone',
+								'jquery': '../libs/jquery/jquery.min'
+							}
+						};
+
+					equal(typeof(sandboxApi.sandboxManager), 'object', 'Can not find link to sandbox manager');
+					equal(typeof(sandboxApi.sandboxManager.nameToUrl), 'function', 'Can not find nameToUrl method in sandbox manager');
+					equal(sandboxApi.sandboxManager.nameToUrl('main', options), 'app/static/js/main', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('view/main', options), 'app/static/js/view/main', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('styles/main', options), 'app/static/js/styles/main', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('css/main', options), 'app/styles/css/main', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('jquery', options), 'app/static/libs/jquery/jquery.min', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('backbone', options), 'app/static/libs/backbone', 'Wrong url resoving');
+					equal(sandboxApi.sandboxManager.nameToUrl('backbone/backbone.min', options), 'app/static/libs/backbone/backbone.min', 'Wrong url resoving');
+				});
+
 				require(['css!style1', 'css!style2', 'view'], function(style1, style2, AppView) {
 					test('css loading test', function() {
 						equal(typeof(style1), 'object', 'Returned module object is not object');
@@ -104,27 +125,6 @@ asyncTest('loading requirejs-sandbox module', 1, function() {
 						equal(instance.globalVar, null, 'App has attr when it should be global variable');
 						equal(window.globalVar, null, 'App global variable scoped to main window object');
 						equal(sandboxApi.sandboxManager.sandbox.globalVar, 'variable', 'App global variable has not scoped to sandbox window object');
-					});
-
-					test('resoving url to file by it\'s name', function() {
-						var options = {
-								baseUrl: 'app/static/js',
-								paths: {
-									'css': '../../styles/css',
-									'backbone': '../libs/backbone',
-									'jquery': '../libs/jquery/jquery.min'
-								}
-							};
-
-						equal(typeof(sandboxApi.sandboxManager), 'object', 'Can not find link to sandbox manager');
-						equal(typeof(sandboxApi.sandboxManager.nameToUrl), 'function', 'Can not find nameToUrl method in sandbox manager');
-						equal(sandboxApi.sandboxManager.nameToUrl('main', options), 'app/static/js/main', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('view/main', options), 'app/static/js/view/main', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('styles/main', options), 'app/static/js/styles/main', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('css/main', options), 'app/styles/css/main', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('jquery', options), 'app/static/libs/jquery/jquery.min', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('backbone', options), 'app/static/libs/backbone', 'Wrong url resoving');
-						equal(sandboxApi.sandboxManager.nameToUrl('backbone/backbone.min', options), 'app/static/libs/backbone/backbone.min', 'Wrong url resoving');
 					});
 				});
 			}
