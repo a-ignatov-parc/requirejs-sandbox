1230d43f04f7cee9649bdff9ad041256b67c4503
Anton Ignatov <abietis@gmail.com>
2014-03-05 13:45:34 +0400

    Added build files to repository and removed script section from bower.json

---
+++ build/patches/jquery.js
@@ -0,0 +1,124 @@
+define('requirejs-sandbox/patches/jquery',['requirejs-sandbox/logger/logger','requirejs-sandbox/helpers/patch'],function(console, patchAbstract) {
+	return patchAbstract.init({
+		// Имя модуля используемое в require.js.
+		name: 'jquery',
+
+		// Имя по которому необходимо искать библиотеку в песочнице, в случае не срабатывания 
+		// резолва по require.shim.exportsFn().
+		shimName: 'jQuery',
+
+		// Метод инициализации патча.
+		enable: function(window, sandbox, jQuery) {
+			var options = this._options,
+				rootEl;
+
+			if (options.rootEl && options.rootEl.nodeType && options.rootEl.nodeType === 1) {
+				rootEl = options.rootEl;
+			}
+
+			// Проверка на существование `jQuery`
+			if (typeof(jQuery) !== 'function') {
+				console.error('This patch require jQuery to be defined!');
+				return;
+			}
+
+			// Переопределяем метод `getComputedStyle` в песочнице чтоб он ссылался на соответсвующий метод в 
+			// основном документе иначе в jquery версии 1.8.x и старше будут не правильно определятся 
+			// видимость элементов и некоторые другие стили.
+			if (sandbox.getComputedStyle && window.getComputedStyle) {
+				sandbox.getComputedStyle = function() {
+					return window.getComputedStyle.apply(window, arguments);
+				};
+			}
+
+			// Проверяем не сделан ли уже патч `jQuery`, если сделан, то выходим.
+			if (jQuery.fn.__patchedInit) {
+				console.debug('jQuery already patched. Skipping...');
+				return;
+			}
+
+			// Делаем обертку над `jQuery.fn.init` для возможносит прозрачного переброса селекторов на 
+			// основную страницу.
+			// 
+			// Возможен небольшой хак чтоб получить `jQuery` объект для песочницы. Необходимо в качестве 
+			// селектора передать объект `window` песочница, так же этот же `window` передать в качестве 
+			// контекста.
+			// 
+			// __Пример:__  
+			// 
+			//         $sandbox = $(sandbox, sandbox);
+			jQuery.fn.init = (function(Fn, proto) {
+				// Запоминаем изначальный метод `init` для последующего его востановления в 
+				// исходное состояние, если потребуется.
+				if (!proto.__originalInit) {
+					proto.__originalInit = Fn;
+				}
+
+				// Создаем новый, пропатченный, метод `init`.
+				proto.__patchedInit = function(selector, context, rootjQuery) {
+					if (typeof(selector) === 'string' && !context) {
+						switch (selector) {
+							case 'html':
+								return new Fn(selector, window.document, rootjQuery);
+							case 'body':
+								return new Fn(rootEl || window.document.body, context, rootjQuery);
+							default:
+								return new Fn(selector, rootEl || window.document, rootjQuery);
+						}
+					} else if (selector == sandbox && context != sandbox) {
+						return new Fn(window, context, rootjQuery);
+					} else if (selector == sandbox.document) {
+						return new Fn(window.document, context, rootjQuery);
+					} else if (selector == sandbox.document.head) {
+						return new Fn(window.document.head, context, rootjQuery);
+					} else if (selector == sandbox.document.body) {
+						return new Fn(rootEl || window.document.body, context, rootjQuery);
+					} else {
+						if (rootEl) {
+							var container = rootEl,
+								contained = context;
+
+							// Так как для проверки на вложенность одного элемента в другой 
+							// необходимы DOM элементы, то мы делаем проверку и получаем их.
+							if (container instanceof jQuery) {
+								container = container[0];
+							}
+
+							if (contained instanceof jQuery) {
+								contained = contained[0];
+							}
+
+							if (jQuery.contains(container, contained)) {
+								return new Fn(selector, context, rootjQuery);
+							} else {
+								return new Fn(selector, rootEl, rootjQuery);
+							}
+						} else {
+							return new Fn(selector, context, rootjQuery);
+						}
+					}
+				};
+
+				// Проставляем у нашей функции-оболочки в качестве прототипа прототип jQuery
+				proto.__patchedInit.prototype = proto;
+				return proto.__patchedInit;
+			})(jQuery.fn.init, jQuery.fn);
+		},
+
+		// Метод отката патча.
+		disable: function(window, sandbox, jQuery) {
+			if (jQuery.fn.__originalInit) {
+				console.debug('Restoring original jQuery instance');
+
+				jQuery.fn.init = jQuery.fn.__originalInit;
+				delete jQuery.fn.__originalInit;
+				delete jQuery.fn.__patchedInit;
+			} else {
+				console.warn('jQuery wasn\'t patched. Skipping...');
+			}
+		}
+	});
+});
+
+define("jquery", function(){});
+
