935eced905dc3c5b028ee501f1555be20174114e
Anton Ignatov <abietis@gmail.com>
2014-04-07 01:53:10 +0400

    Prefix regex changed to more reliable version

--- src/helpers/processor/prefix.js
+++ src/helpers/processor/prefix.js
@@ -1,12 +1,21 @@
 define([
 	'logger/logger'
 ], function(console) {
-	var selectorsRegex = /([},]\s*)([^@][.#[*]?[^,{]+)/g;
+	// Регулярное выражение для поиска селекторов и разбивание на группы для последующего 
+	// проставления префикса.
+	// 
+	// Найденные проблемы:
+	// 1. Не заменяются селекторы body и html на селектор префикса.
+	// 2. Нет явного исключения селекторов "@xxx"
+	var selectorsRegex = /(^|\s+|}\s*)([*.#\w\d\s-:\[\]\(\)="]+)(,|{[^}]+})/g;
 
 	return {
 		prefix: function(selector) {
-			this.responseSourceCache[this.id] = ('}' + this.responseSourceCache[this.id]).replace(selectorsRegex, '$1' + selector + ' $2');
-			this.responseSourceCache[this.id] = this.responseSourceCache[this.id].substr(1);
+			// Удаляем все коментарии из кода, они все равно только мешаются.
+			this.responseSourceCache[this.id] = this.responseSourceCache[this.id].replace(/\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*\/+/g, '');
+
+			// Проставляем префикс у селекторов.
+			this.responseSourceCache[this.id] = this.responseSourceCache[this.id].replace(selectorsRegex, '$1' + selector + ' $2$3');
 			console.debug('[prefix] Executing result for selector "' + selector + '": ', this.responseSourceCache[this.id]);
 			return this;
 		}
