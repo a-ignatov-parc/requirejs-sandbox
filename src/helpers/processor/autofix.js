5491b351db94146e10afba0c242baf7b8b5f2fe5
Anton Ignatov <abietis@gmail.com>
2014-06-20 15:42:23 +0400

    Fixed jslint error

--- src/helpers/processor/autofix.js
+++ src/helpers/processor/autofix.js
@@ -6,7 +6,7 @@ define(function() {
 				originalSourceCode = this._responseSourceCache[this.id];
 
 			for (var i = 0, length = propertyList.length; i < length; i++) {
-				fixedSourceCode += 'var ' + propertyList[i] + ' = window.sandboxApi && window.sandboxApi.parentWindow.' + propertyList[i] + ' || window.' + propertyList[i] + ';'
+				fixedSourceCode += 'var ' + propertyList[i] + ' = window.sandboxApi && window.sandboxApi.parentWindow.' + propertyList[i] + ' || window.' + propertyList[i] + ';';
 				originalSourceCode.replace('window.' + propertyList[i], propertyList[i]);
 			}
 			this._responseSourceCache[this.id] = fixedSourceCode + originalSourceCode;
